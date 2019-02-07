69151366fb27d54d66b49c94fc0a384dd5b969ef
minhnln <mingngo158@gmail.com>
2019-02-08 01:44:49 +0700

    Combine build process and bridge the combination

--- src/main/java/com/taskmanagement/AppApplication.java
+++ src/main/java/com/taskmanagement/AppApplication.java
@@ -1,4 +1,4 @@
-package com.taskmanagement.app;
+package com.taskmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/taskmanagement/AppApplicationTests.java
+++ src/test/java/com/taskmanagement/AppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.taskmanagement.app;
+package com.taskmanagement;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
