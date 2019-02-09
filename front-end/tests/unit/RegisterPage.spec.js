b7bf73ebc7ea3e8fe2d35d2b1e20a6e586398de3
minhnln <mingngo158@gmail.com>
2019-02-09 09:14:15 +0700

    Create register page

---
+++ front-end/tests/unit/RegisterPage.spec.js
@@ -0,0 +1,55 @@
+import { mount } from '@vue/test-utils'
+import RegisterPage from '@/views/RegisterPage'
+
+describe('RegisterPage.vue', () => {
+  let wrapper
+  let fieldUsername
+  let fieldEmailAddress
+  let fieldPassword
+  let buttonSubmit
+
+  beforeEach(() => {
+    wrapper = mount(RegisterPage)
+    fieldUsername = wrapper.find('#username')
+    fieldEmailAddress = wrapper.find('#emailAddress')
+    fieldPassword = wrapper.find('#password')
+    buttonSubmit = wrapper.find('form button[type="submit"]')
+  })
+
+  it('should render registration form correctly', () => {
+    expect(wrapper.find('.logo').attributes().src)
+      .toEqual('/static/images/logo.png')
+    expect(wrapper.find('.tagline').text())
+      .toEqual('Open source task management tool')
+    expect(fieldUsername.element.value).toEqual('')
+    expect(fieldEmailAddress.element.value).toEqual('')
+    expect(fieldPassword.element.value).toEqual('')
+    expect(buttonSubmit.text()).toEqual('Create account')
+  })
+
+  it('should contain data model with initial value', () => {
+    expect(wrapper.vm.form.username).toEqual('')
+    expect(wrapper.vm.form.emailAddress).toEqual('')
+    expect(wrapper.vm.form.password).toEqual('')
+  })
+
+  it('should have form inputs bound with data model', () => {
+    const username = 'username'
+    const emailAddress = 'emailAddress@gmail.com'
+    const password = 'password'
+
+    wrapper.vm.form.username = username
+    wrapper.vm.form.emailAddress = emailAddress
+    wrapper.vm.form.password = password
+    expect(fieldUsername.element.value).toEqual(username)
+    expect(fieldEmailAddress.element.value).toEqual(emailAddress)
+    expect(fieldPassword.element.value).toEqual(password)
+  })
+
+  it('should have form submit event handler `submitForm`', () => {
+    const stub = jest.fn()
+    wrapper.setMethods({ submitForm: stub })
+    buttonSubmit.trigger('submit')
+    expect(stub).toBeCalled()
+  })
+})
